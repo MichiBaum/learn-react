0dea41b3a1a4ce59e4fc62d5c17b59aa9a966263
Michael Baumberger <michael_baumberger@gmx.ch>
2024-11-15 16:47:29 +0100

    Add PrimeReact and create basic Header and Body components
    Integrated `primereact` for UI components and updated `package.json` and `package-lock.json`. Implemented and included basic `Header` and `Body` components within the `App` component using `PrimeReactProvider`.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
+import {PrimeReactProvider} from "primereact/api";
+import Header from "./components/header/Header.tsx";
+import Body from "./components/body/Body.tsx";
 
 function App() {
 
   return (
     <>
-      <p>Working</p>
+        <PrimeReactProvider>
+            <Header></Header>
+            <Body></Body>
+        </PrimeReactProvider>
     </>
   )
 }
